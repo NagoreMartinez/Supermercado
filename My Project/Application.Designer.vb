e854d0f5b96f910419b8aa0a920b7a332a242f00
Nagore <ikazc@plaiaundi.net>
2020-05-10 00:27:12 +0200

    Diseños básicos de formularios

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Supermercado.Form1
+            Me.MainForm = Global.Supermercado.FormEmpleados
         End Sub
     End Class
 End Namespace
